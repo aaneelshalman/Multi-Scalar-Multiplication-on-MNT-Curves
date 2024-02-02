d9f57c692aa8c8d927bd3c1958c755c284dd54ad
aaneelshalman <aaneelshalmans@gmail.com>
2024-02-02 09:07:22 +0000

    Added test for NAF decomposition

--- msm/src/main.rs
+++ msm/src/main.rs
@@ -8,6 +8,7 @@ mod operations;
 mod pippenger;
 mod parallel_pippenger;
 mod naive;
+mod naf_pippenger;
 
 use ark_mnt4_298::G1Projective;
 use ark_std::{UniformRand, test_rng};
@@ -16,6 +17,7 @@ use std::time::Instant;
 use parallel_pippenger::parallel_pippenger;
 use pippenger::pippenger;
 use naive::naive_msm;
+use naf_pippenger::naf_pippenger;
 // use operations::{add_points, scalar_multiply};
 
 fn main() {
@@ -30,9 +32,9 @@ fn main() {
         (0..num_scalars).map(|_| rng.gen_range(0..65536)).collect()
     }
 
-    let points = generate_points(1000);  // Generate 1000 points
-    let scalars = generate_scalars(1000); // Generate 1000 scalars
-    let window_size = 2; // Example window size
+    let points = generate_points(1000);
+    let scalars = generate_scalars(1000);
+    let window_size = 2;
 
     let start = Instant::now();
     let result_naive = naive_msm(&points, &scalars);
@@ -46,10 +48,16 @@ fn main() {
     let result_parallel = parallel_pippenger(&points, &scalars, window_size);
     let duration_parallel = start.elapsed();
 
+    let start = Instant::now();
+    let result_naf = naf_pippenger(&points, &scalars, window_size);
+    let duration_naf = start.elapsed();
+
     assert_eq!(result_naive, result_pippenger, "Results of pippenger and naive MSM should match");
     assert_eq!(result_naive, result_parallel, "Results of pippenger with parallelism and naive MSM should match");
+    assert_eq!(result_naive, result_naf, "Results of pippenger with 2-NAF Decomposition and parallelism should match with naive MSM");
 
     println!("Naive Execution Time: {:?}", duration_naive);
     println!("Pippenger Execution Time: {:?}", duration_pippenger);
     println!("Parallel Execution Time: {:?}", duration_parallel);
+    println!("NAF Execution Time: {:?}", duration_naf);
 }
